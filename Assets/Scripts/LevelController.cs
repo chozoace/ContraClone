882b151348e70624eb6152a87d126b15d3affc80
Roger Rosa <chozoace@gmail.com>
2019-09-02 17:29:53 -0400

    level updates

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelController
+public class LevelController : MonoBehaviour
 {
     //This controller will change according to scene
     //This is really more of a scene controller
@@ -14,6 +14,12 @@ public class LevelController
     //level controller only updates active partitions
     //Game state, Pause state, menu state. Each will have it's own controller.
     //A controller for every state
+    //https://www.habrador.com/tutorials/programming-patterns/19-spatial-partition-pattern/
+    public void Start()
+    {
+        sceneWidth = GameObject.FindGameObjectWithTag("LevelEnd").transform.position.x;
+    }
+
     public void CreateWorld()
     {
 
