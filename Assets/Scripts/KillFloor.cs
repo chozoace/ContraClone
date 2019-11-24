95fd0fce77b574cbf9ff100ead427a3d8c57194e
Roger Rosa <chozoace@gmail.com>
2019-11-24 18:02:10 -0500

    Enemy Death implemented

---
+++ Assets/Scripts/KillFloor.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillFloor : MonoBehaviour
+{
+    float damage = 9001;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("enter");
+        collision.GetComponent<IDestructable>()?.TakeDamage(damage);
+    }
+}
